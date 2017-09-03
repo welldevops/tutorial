859e9172d29e8547e0ec5660d29690466e8d06df
Tunde Oladipupo <tunde.oladipupo@kibocommerce.com>
2017-09-03 16:29:33 -0500

    looking into password security

--- pipeline1/jenkins/scripts/security.groovy
+++ pipeline1/jenkins/scripts/security.groovy
@@ -6,8 +6,8 @@ import jenkins.security.s2m.AdminWhitelistRule
 
 def instance = Jenkins.getInstance()
 
-def user = new File("/run/secrets/jenkins-user").text.trim()
-def pass = new File("/run/secrets/jenkins-pass").text.trim()
+def user = new File("admin").text.trim()
+def pass = new File("admin").text.trim()
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 hudsonRealm.createAccount(user, pass)
